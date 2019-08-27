d272e428c4f7bccb346ceef13c26409636fc89a7
zhc <me@zhanghuichu.com>
2019-08-27 17:13:06 +0800

    add args

--- run.py
+++ run.py
@@ -4,28 +4,42 @@ from world import World
 from generator import LaneVehicleGenerator
 from agent import RLAgent
 from metric import TravelTimeMetric
+import argparse
 
-world = World("examples/config.json", thread_num=1)
+# parse args
+parser = argparse.ArgumentParser(description='Run Example')
+parser.add_argument('config_file', type=str, help='path of config file')
+parser.add_argument('--thread', type=int, help='number of threads')
+parser.add_argument('--steps', type=int, default=100, help='number of steps')
+args = parser.parse_args()
 
+# create world
+world = World(args.config_file, thread_num=args.thread)
+
+# create agents
 agents = []
 for i in world.intersections:
     action_space = gym.spaces.Discrete(len(i["trafficLight"]["lightphases"]))
     agents.append(RLAgent(
         action_space, 
-        LaneVehicleGenerator(world, i["id"], ["lane_count"], in_only=True),
+        LaneVehicleGenerator(world, i["id"], ["lane_count"], in_only=True, average="road"),
         LaneVehicleGenerator(world, i["id"], ["lane_waiting_count"], in_only=True, average="all", negative=True)
     ))
 
+# create metric
 metric = TravelTimeMetric(world)
+
+# create env
 env = TSCEnv(world, agents, metric)
 
+# simulate
 obs = env.reset()
-for i in range(100):
+for i in range(args.steps):
     if i % 5 == 0:
         actions = env.action_space.sample()
     obs, rewards, dones, info = env.step(actions)
     #print(obs)
-    print(rewards)
+    #print(rewards)
     print(info["metric"])
 
 print("Final Travel Time is %.4f" % env.metric.update(done=True))
